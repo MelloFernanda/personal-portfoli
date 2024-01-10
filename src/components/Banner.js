5c9e4cb2743d07a7ee09a2ff0cc18ea1cbb96b05
FernandaMello <fernandam04@outlook.com>
2024-01-10 10:24:41 -0300

    banner animation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 
 import headerImg from "../assets/img/header-img.svg"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Banner = () => {
 
@@ -10,8 +10,42 @@ export const Banner = () => {
     const [isDeleting, setIsDeleting] = useState(false);
     const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
     const [text, setText] = useState('');
+    const [index, setIndex] = useState(1);
     const [delta, setDelta] = useState(300 - Math.random * 100) ;
     const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+          tick();
+        }, delta);
+    
+        return () => { clearInterval(ticker) };
+      }, [text])
+    
+      const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    
+        setText(updatedText);
+    
+        if (isDeleting) {
+          setDelta(prevDelta => prevDelta / 2);
+        }
+    
+        if (!isDeleting && updatedText === fullText) {
+          setIsDeleting(true);
+          setIndex(prevIndex => prevIndex - 1);
+          setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+          setIsDeleting(false);
+          setLoopNum(loopNum + 1);
+          setIndex(1);
+          setDelta(500);
+        } else {
+          setIndex(prevIndex => prevIndex + 1);
+        }
+      }
 
     return(
         <section className="banner" id="home">
@@ -19,7 +53,7 @@ export const Banner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Bem-vindo ao meu portifólio</span>
-                        <h1>{`Hi I'm webdecoded`}<spam className="wrap">web developer</spam></h1>
+                        <h1>{`Hi I'm Fernanda Mello `}<spam className="wrap">{text}</spam></h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
                         <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/> </button>
                     </Col>
