ffd9df0b6d655f3744d4ab7916a672a51e52e4de
FernandaMello <fernandam04@outlook.com>
2024-01-10 09:58:39 -0300

    Banner initial code

---
+++ src/components/Banner.js
@@ -0,0 +1,24 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+
+import headerImg from "../assets/img/header-img.svg"
+
+export const Banner = () => {
+    return(
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Bem-vindo ao meu portifólio</span>
+                        <h1>{`Hi I'm webdecoded`}<spam className="wrap">web developer</spam></h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/> </button>
+                    </Col>
+                    <Col s={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Headder Img"/>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
