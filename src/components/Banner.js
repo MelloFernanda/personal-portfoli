8b001ddc83db4b3e17128f650823c21f376b5c2b
FernandaMello <fernandam04@outlook.com>
2024-01-15 13:47:15 -0300

    adjust text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -52,7 +52,7 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Bem-vindo ao meu portifólio</span>
+                        <span className="tagline">Bem-vindo ao meu portfólio</span>
                         <h1>{`Oi, sou a Fernanda: `}<spam className="wrap"><br></br>{text}</spam></h1>
                         <p>Olá! Sou uma entusiasta de desenvolvimento, apaixonada por desafios cósmicos e pronta para explorar novos horizontes.<br></br>Minha abordagem é minuciosa, 'cricri' quando se trata de estética, pois acredito que cada detalhe importa, assim como as estrelas que compõem o universo. Encaro a vida como uma jornada intergaláctica, onde o pior medo é permanecer com receio das mudanças. Estou pronta para romper com fronteiras e abraçar o desconhecido, pois, afinal, a verdadeira transformação só acontece quando ousamos mudar.</p>
                         <a href='#connect'><button onClick={() => console.log('connect')}>Vamos nos conectar? <ArrowRightCircle size={25}/> </button></a>
